a483cfcb7eb0159410bd362d7c0c9baa6ae588cc
Amin Mousavi <ma.mousavi97@gmail.com>
2025-09-08 21:46:50 +0300

    added mlflow functionality

--- train.py
+++ train.py
@@ -1,12 +1,50 @@
 import joblib
+import mlflow
+from mlflow.models import infer_signature
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
 
 X, y = load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-model = LogisticRegression()
+params = {
+    "solver": "lbfgs",
+    "max_iter": 1000,
+    "multi_class": "auto",
+    "random_state": 42,
+}
+
+model = LogisticRegression(**params)
 model.fit(X_train, y_train)
 
-joblib.dump(model, "model.pkl")
+
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+
+mlflow.set_tracking_uri("http://localhost:8080")
+mlflow.set_experiment("Iris Classification")
+
+with mlflow.start_run():
+    mlflow.log_params(params)
+
+    mlflow.log_metric("accuracy", accuracy)
+
+    signature = infer_signature(X_train, model.predict(X_train))
+
+    model_info = mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="iris_model",
+        signature=signature,
+        input_example=X_train,
+    )
+
+
+    mlflow.set_tags({"Training Info": "Basic LogReg model for iris dataset"})
+
+
+loaded_model = mlflow.pyfunc.load_model(model_info.model_uri)
+predictions = loaded_model.predict(X_test)
+print(predictions[:10])
